5eeabaef0d96879932da26e3c5cea506c7a3df4b
alfred82santa <alfred82santa@gmail.com>
2014-02-05 08:40:22 +0100

    PEP8

--- tests/dirty_validators/tests_complex.py
+++ tests/dirty_validators/tests_complex.py
@@ -14,28 +14,28 @@ class TestChainStopOnFail(TestCase):
     def test_validate_str_success(self):
         self.assertTrue(self.validator.is_valid('abcdefg@test.com'))
         self.assertDictEqual(self.validator.messages, {})
-        
+
     def test_validate_str_message_first_validator_fail_1(self):
         self.assertFalse(self.validator.is_valid('abcdefghijk@test.com'))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {Length.TOO_LONG: "'abcdefghijk@test.com' is more than 16 unit length"})
-                             
+
     def test_validate_str_message_first_validator_fail_2(self):
         self.assertFalse(self.validator.is_valid('abc@test.com'))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {Length.TOO_SHORT: "'abc@test.com' is less than 14 unit length"})
-                             
+
     def test_validate_str_message_second_validator_fail(self):
         self.assertFalse(self.validator.is_valid('abfghi@test.com'))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {Regexp.NOT_MATCH: "'abfghi@test.com' does not match against pattern '^abc'"})
-                             
+
     def test_validate_str_message_third_validator_fail(self):
         self.assertFalse(self.validator.is_valid('abcdefg+test.com'))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {Email.NOT_MAIL: "'abcdefg+test.com' is not a valid email address."})
-                             
-                             
+
+
 class TestChainDontStopOnFail(TestCase):
 
     def setUp(self):
@@ -47,15 +47,15 @@ class TestChainDontStopOnFail(TestCase):
     def test_validate_str_success(self):
         self.assertTrue(self.validator.is_valid('abcdefg@test.com'))
         self.assertDictEqual(self.validator.messages, {})
-        
+
     def test_validate_str_fail_all(self):
         self.assertFalse(self.validator.is_valid('abadefghijk+test.com'))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {Length.TOO_LONG: "'abadefghijk+test.com' is more than 16 unit length",
                               Regexp.NOT_MATCH: "'abadefghijk+test.com' does not match against pattern '^abc'",
                               Email.NOT_MAIL: "'abadefghijk+test.com' is not a valid email address."})
-                              
-                              
+
+
 class TestAllItemsStopOnFail(TestCase):
 
     def setUp(self):
@@ -67,23 +67,23 @@ class TestAllItemsStopOnFail(TestCase):
     def test_validate_success(self):
         self.assertTrue(self.validator.is_valid(['abcdefg@test.com', '12345678901234', 'abcdefghijklmno']))
         self.assertDictEqual(self.validator.messages, {})
-        
+
     def test_validate_first_fail(self):
         self.assertFalse(self.validator.is_valid(['test', '12345678901234', 'abcdefghijklmno']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {0: {Length.TOO_SHORT: "'test' is less than 14 unit length"}})
-        
+
     def test_validate_second_fail(self):
         self.assertFalse(self.validator.is_valid(['abcdefg@test.com', '12345678901234567', 'abcdefghijklmno']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {1: {Length.TOO_LONG: "'12345678901234567' is more than 16 unit length"}})
-                             
+
     def test_validate_first_and_last_fail(self):
         self.assertFalse(self.validator.is_valid(['test', '12345678901234', 'abcdefghijklmnsssssssso']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {0: {Length.TOO_SHORT: "'test' is less than 14 unit length"}})
-                             
-                             
+
+
 class TestAllItemsDontStopOnFail(TestCase):
 
     def setUp(self):
@@ -91,15 +91,15 @@ class TestAllItemsDontStopOnFail(TestCase):
 
     def tearDown(self):
         pass
-                             
+
     def test_validate_all_fail(self):
         self.assertFalse(self.validator.is_valid(['test', '12345678901234567', 'abcdefghijklmnsssssssso']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {0: {Length.TOO_SHORT: "'test' is less than 14 unit length"},
                               1: {Length.TOO_LONG: "'12345678901234567' is more than 16 unit length"},
                               2: {Length.TOO_LONG: "'abcdefghijklmnsssssssso' is more than 16 unit length"}})
-                              
-                              
+
+
 class TestSomeItems(TestCase):
 
     def setUp(self):
@@ -111,25 +111,23 @@ class TestSomeItems(TestCase):
     def test_validate_success(self):
         self.assertTrue(self.validator.is_valid(['abcde', '12345678901234', 'abcd', 'qawsw']), self.validator.messages)
         self.assertDictEqual(self.validator.messages, {})
-        
+
     def test_validate_too_few_items_fail(self):
         self.assertFalse(self.validator.is_valid(['tes', '1234', 'abcdefghijklmno']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {0: {Length.TOO_SHORT: "'tes' is less than 4 unit length"},
                               2: {Length.TOO_LONG: "'abcdefghijklmno' is more than 6 unit length"},
                               SomeItems.TOO_FEW_VALID_ITEMS: "Too few items pass validation"})
-        
+
     def test_validate_too_many_items_stop_on_fail(self):
         self.assertFalse(self.validator.is_valid(['test', '12345', 'asaa', 'abcde', 'wewwwwww', 'sd']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {SomeItems.TOO_MANY_VALID_ITEMS: "Too many items pass validation"})
-        
+
     def test_validate_too_many_items_dont_stop_on_fail(self):
         self.validator = SomeItems(min=1, max=2, stop_on_fail=False, validator=Length(min=4, max=6))
         self.assertFalse(self.validator.is_valid(['test', '12345', 'abcde', 'wewwwwww', 'sd', '1qaz', '2wsx']))
-        self.assertDictEqual(self.validator.messages, 
+        self.assertDictEqual(self.validator.messages,
                              {SomeItems.TOO_MANY_VALID_ITEMS: "Too many items pass validation",
                               4: {Length.TOO_SHORT: "'sd' is less than 4 unit length"},
                               3: {Length.TOO_LONG: "'wewwwwww' is more than 6 unit length"}})
-                             
-   
