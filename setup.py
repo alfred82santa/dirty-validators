ffe06c9c644b040a4a1baa03f86b45ad67396d1a
Alfred Santacatalina Gea <alfred82santa@gmail.com>
2014-05-07 14:48:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dirty-validators',
     url='https://github.com/alfred82santa/dirty-validators',
     author='alfred82santa',
-    version='0.1.1',
+    version='0.1.2',
     author_email='alfred82santa@gmail.com',
     packages=['dirty_validators'],
     include_package_data=True,
